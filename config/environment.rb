426f825f93ab39e7b99350e968b6cf071dfa64ab
Juan Trejo <trejocola@gmail.com>
2020-04-05 01:24:34 -0600

    puts database_url

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler'
 Bundler.require
+puts "=========================> database URL: #{ENV['DATABASE_URL']}"
 db_config = YAML.load_file('config/database.yml')
 ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
