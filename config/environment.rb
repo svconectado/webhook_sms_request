b235679a4edaaf6f91685e81fbfeae79a7d6b390
Juan Trejo <trejocola@gmail.com>
2020-04-05 04:33:00 -0600

    remove lines

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,4 @@
 require 'bundler'
 Bundler.require
-puts "=========================> database URL: #{ENV['DATABASE_URL']} printed DB"
-
 db_config = YAML.load_file('config/database.yml')
 ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
