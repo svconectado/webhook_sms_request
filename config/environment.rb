212b688e4c767de9a5fe240622b51f92d1780d00
Juan Trejo <trejocola@gmail.com>
2020-04-05 04:34:20 -0600

    add logging line

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler'
 Bundler.require
 db_config = YAML.load_file('config/database.yml')
+puts "======================================> db_config line value #{db_config.inspect}"
 ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
